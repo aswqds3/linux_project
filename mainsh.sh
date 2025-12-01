a46f8f661511e680a571a190c90d4570a2b90795
LYJ <yongjin9280@inu.ac.kr>
2025-12-01 16:12:44 +0900

    셸스크립트 기본 기능 구현

--- mainsh.sh
+++ mainsh.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
-# 영단어 학습 프로그램 셸 인터페이스
-
 echo "$USERNAME 님 반갑습니다!"
 echo "영단어 학습 프로그램을 시작합니다."
 
 while true; do
     echo ""
-    echo "사용 가능한 명령어: show / exit"
+    echo "사용 가능한 명령어: show / add / quiz / check / exit"
     read -p "명령어를 입력하세요: " command
 
-    if [ "$command" = "show" ]; then
-        echo "기능: 영단어 표시"
-        ./main 0
-    elif [ "$command" = "exit" ]; then
-        echo "프로그램을 종료합니다."
-        break
-    else
-        echo "알 수 없는 명령어입니다. 다시 입력해주세요."
-    fi
+    case "$command" in
+        show)
+            echo "기능: 영단어 표시"
+            ./main_program 0
+            ;;
+        add)
+            echo "기능: 영단어 추가"
+            ./main_program 1
+            ;;
+        quiz)
+            echo "기능: 영단어 퀴즈"
+            ./main_program 2
+            ;;
+        check)
+            echo "기능: 학습 진도표 체크"
+            ./main_program 3
+            ;;
+        exit)
+            echo "프로그램을 종료합니다."
+            break
+            ;;
+        *)
+            echo "알 수 없는 명령어입니다. 다시 입력해주세요."
+            ;;
+    esac
 done
